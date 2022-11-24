7ab7c59d1d603d1d64e06f43c4440de33b2c7306
Matthew <matt@pandolfo.com>
2022-11-23 22:33:16 -0500

    footer links and view widths

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,46 +12,58 @@ export default function Contact() {
   });
 
   return (
-    <div id="contact" className="contact-container">
-      <h2>Contact Us</h2>
-      <div className="contact-form-container">
-        <form className="contact-form">
-          <div className="contact-div1">
-            <TextInput className="contact-name" placeholder="Name" variant="unstyled"/>
-            <TextInput className="contact-email" placeholder="Email" variant="unstyled"/>
-          </div>
-          <div className="contact-div2">
-            <TextInput
-              className="contact-subject"
-              placeholder="Subject"
-              variant="unstyled"
-            />
-            <TextInput
-              className="contact-phone"
-              placeholder="Phone"
-              variant="unstyled"
-            />
-            <Textarea
-              className="contact-message"
-              placeholder="Message"
-              variant="unstyled"
-              minRows={5}
-            />
-          </div>
-          <UnstyledButton className="contact-send">Send</UnstyledButton>
-        </form>
-      </div>
-      <footer className="footer-container">
-      <div className="footer-content">
-        <div>
-          <p>&copy; 2022 MJP Systems</p>
+    <div id="contact">
+      <div className="contact-container">
+        <h2>Contact Us</h2>
+        <div className="contact-form-container">
+          <form className="contact-form">
+            <div className="contact-div1">
+              <TextInput
+                className="contact-name"
+                placeholder="Name *"
+                variant="unstyled"
+              />
+              <TextInput
+                className="contact-email"
+                placeholder="Email *"
+                variant="unstyled"
+              />
+            </div>
+            <div className="contact-div2">
+              <TextInput
+                className="contact-subject"
+                placeholder="Subject"
+                variant="unstyled"
+              />
+              <TextInput
+                className="contact-phone"
+                placeholder="Phone"
+                variant="unstyled"
+              />
+              <Textarea
+                className="contact-message"
+                placeholder="Message *"
+                variant="unstyled"
+                minRows={5}
+              />
+            </div>
+            <UnstyledButton className="contact-send">Send</UnstyledButton>
+          </form>
         </div>
-        <div className="footer-contact">
-          <p>Cell: 860-944-9761</p>
-          <p>Email: markp@mjpsystems.com</p>
-        </div>
+        <footer className="footer-container">
+          <div className="footer-content">
+            <div>
+              <p>&copy; 2022 MJP Systems</p>
+            </div>
+            <div className="footer-contact">
+              <p>
+                Cell: <a href="tel:8609449761">860-944-9761</a>
+              </p>
+              <p>Email: <a href="mailto:markp@mjpsystems.com">markp@mjpsystems.com</a></p>
+            </div>
+          </div>
+        </footer>
       </div>
-    </footer>
     </div>
   );
 }
