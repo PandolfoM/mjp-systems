1242586f6994217573faa5278e353e8e5273abc7
Matthew Pandolfo <matt@pandolfo.com>
2023-01-31 08:27:54 -0500

    simplify nav and hero

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -5,16 +5,11 @@ function Hero() {
     <div className="hero-container">
       <div className="hero-img">
         <div className="hero-content">
-          <h1>IT Consulting & Services</h1>
+          <h1>Managed IT Services</h1>
           <hr />
           <p>
-            MJP Systems provides services to help you assess the needs of your
-            business and then implement solutions for maximum efficiency and
-            results. Whether it’s a network support issue, a hardware
-            maintenance, or cloud services—our professional services deliver
-            extensive technical expertise and comprehensive project management
-            to complete the work efficiently and effectively, with the highest
-            level of service.
+            MJP Systems provides Managed IT Services to help small and
+            medium-sized businesses with all their technology needs.
           </p>
         </div>
       </div>
