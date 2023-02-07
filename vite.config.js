4b424a149e9cb8baed1ebdf25c53ca2261b73d9a
Matthew Pandolfo <matt@pandolfo.com>
2023-02-07 08:24:28 -0500

    homepage

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/resetform": "http://localhost:5000",
-      "/contactform": "http://localhost:5000",
-      "/validateClientAccess": "http://localhost:5000",
-      "/isLoggedIn": "http://localhost:5000",
-    },
-  },
+  base: "https://mjpsystems.com/",
 });
